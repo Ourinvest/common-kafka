81bbdec5a41eb5e2371feb1de83ae259c671f382
conrado-silva <conrado.silva@ourinvest.com.br>
2022-04-20 09:13:29 -0300

    feat: standard consumer in a container

--- common-kafka/producers/standard_producer.py
+++ common-kafka/producers/standard_producer.py
@@ -7,20 +7,23 @@ from random import randint
 
 # env variables
 KAFKA_TOPIC = os.getenv('KAFKA_TOPIC')
-KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'kafka:9092')
+KAFKA_BOOTSTRAP_SERVERS = os.getenv(
+    'KAFKA_BOOTSTRAP_SERVERS', 'kafka:29092')
 
 # global variables
 loop = asyncio.get_event_loop()
 
 
 async def send_one():
-    producer = AIOKafkaProducer(loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
+    producer = AIOKafkaProducer(
+        loop=loop, bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS)
     # get cluster layout and initial topic/partition leadership information
     await producer.start()
     try:
         # produce message
         msg_id = f'{randint(1, 10000)}'
-        value = {'message_id': msg_id, 'text': 'some text', 'state': randint(1, 100)}
+        value = {'message_id': msg_id,
+                 'text': 'some text', 'state': randint(1, 100)}
         print(f'Sending message with value: {value}')
         value_json = json.dumps(value).encode('utf-8')
         await producer.send_and_wait(KAFKA_TOPIC, value_json)
